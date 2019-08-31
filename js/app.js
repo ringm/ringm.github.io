afb5030a60c5dbf71d68f4fd685db962ff8bc7c4
Martin Ring <ring.martin@gmail.com>
2019-08-31 14:24:16 -0300

    finishing touches

--- js/app.js
+++ js/app.js
@@ -1,4 +1,8 @@
 
+//--------------------------------------
+//TEXT HOVER
+//--------------------------------------
+
 var textOverImages = document.getElementsByClassName("project");
 var previousTextOverImage;
 
@@ -34,3 +38,23 @@ for (var i = 0; i < textOverImages.length; i++) {
 function stopPropagation(event){
   event.stopPropagation();
 }
+
+
+//--------------------------------------
+//CHECK FORM EMPTY FIELDS 
+//--------------------------------------
+
+const inputUser = document.querySelectorAll('.form-control');
+const sendBtn = document.querySelector('.form-btn');
+const missingFieldUser = document.querySelectorAll('.warning-msg');
+
+sendBtn.addEventListener('click', e => {
+  e.preventDefault();
+  for(let i = 0; i < inputUser.length; i++){
+  if(inputUser[i].value == ''){
+      missingFieldUser[i].classList.remove('hidden');
+      missingFieldUser[i].classList.add('show');
+      } 
+    }
+  }
+)
